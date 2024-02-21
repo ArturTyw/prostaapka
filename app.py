a5ed5d26036302ba4e11ec80c01b65382e89286b
ArturTyw <116441273+ArturTyw@users.noreply.github.com>
2024-02-21 22:56:50 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Witaj, świecie! To jest moja pierwsza aplikacja webowa z użyciem Flask.'
+    return 'Witaj, świecie! Pozdrawiam Pana Michała!'
 
 if __name__ == '__main__':
     app.run(debug=True)
